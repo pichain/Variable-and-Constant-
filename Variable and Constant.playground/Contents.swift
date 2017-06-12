77df190eb01712c8d6b45e3bbae8e68d73425e56
student02 <pichainjunpoom@gmail.com>
2017-06-13 04:52:47 +0700

    print

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -7,11 +7,14 @@ import UIKit
 let myConstant = 123
 // myConstant = 456 มี error
 // การประกาศค่าตัวแปรแบบเปลี่ยนค่าได้
-var intNumber = 123 // การประกาศค่าตัวแปร แบบเปบี่ยนค่าได้
+var intNumber = 123 // การประกาศค่าตัวแปร แบบเปลี่ยนค่าได้
 intNumber = 345
 // Explicit การประกาศค่าตัวแปรแบบกำหนดค่า datatype
 let myconstant1: Int = 123
-
+// Display on console
+print(intNumber)
+//Display String and Value
+print("intNumber ==> \(intNumber)")
 
 
 
