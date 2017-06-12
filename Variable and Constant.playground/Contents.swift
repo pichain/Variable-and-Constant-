e5be0754aaddc7c799220cae63e4f6563e9696b2
student02 <pichainjunpoom@gmail.com>
2017-06-13 04:23:06 +0700

    Variable

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This Comment 
+// การประกาศตัวแปรแบบไม่สามารถเปลี่ยนค่าได้
+let myConstant = 123
+// myConstant = 456 มี error
+// การประกาศค่าตัวแปรแบบเปลี่ยนค่าได้
+var intNumber = 123 // การประกาศค่าตัวแปร แบบเปบี่ยนค่าได้
+intNumber = 345
+// Explicit การประกาศค่าตัวแปรแบบกำหนดค่า datatype
+let myconstant1: Int = 123
+
+
+
+
